7924d76dece5ae500235a96d99e73c18fc2f1670
fp90697 <fp90697@linecorp.com>
2020-09-22 03:35:28 +0900

    study upt

--- webpack-middle-class/src/app.js
+++ webpack-middle-class/src/app.js
@@ -1,2 +1,6 @@
-import * as math from './math.js';
-console.log(math.sum(1, 2));
+import './app.css';
+import nyancat from './nyancat.jpg';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `<img src="${nyancat}">`;
+})
