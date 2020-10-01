6255bee5a0034d84c5c960d8c2ba60eaab8745c0
fp90697 <fp90697@linecorp.com>
2020-10-01 10:12:34 +0900

    webpack upt

--- webpack-middle-class/webpack.config.js
+++ webpack-middle-class/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const MyWebpackPlugin = require('./my-webpack-plugin');
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -23,7 +25,7 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)$/,
                 loader: 'url-loader',
                 options: {
-                    publicPath: './dist',
+                    // publicPath: './dist',
                     name: '[name].[ext]?[hash]',
                     limit: 20000, // 20kb
                 }
@@ -31,6 +33,26 @@ module.exports = {
         ]
     },
     plugins: [
-        new MyWebpackPlugin(),
+        new webpack.BannerPlugin({
+            banner: `
+                Build Date: ${new Date().toLocaleString()}
+                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+                Author: ${childProcess.execSync('git config user.name')}
+            `
+        }),
+        new webpack.DefinePlugin({
+            TWO: JSON.stringify('1+1'),
+            'api.domain': JSON.stringify('http://dev.api.domain.com')
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            templateParameters: {
+                env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+            },
+            minify: process.env.NODE_ENV === 'production' ? {
+                collapseWhitespace: true,
+                removeComments: true,
+            } : false
+        })
     ]
 }
