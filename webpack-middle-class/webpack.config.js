5dbbfbe465a86383b44941e5b93173733a729d46
fp90697 <fp90697@linecorp.com>
2020-10-02 23:05:55 +0900

    study upt

--- webpack-middle-class/webpack.config.js
+++ webpack-middle-class/webpack.config.js
@@ -8,11 +8,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: "development",
     entry: {
-        main: "./app.js"
+        main: "./src/app.js"
     },
     output: {
         path: path.resolve('./dist'),
         filename: "[name].js" // name - 위에 main 키네임을 가져온다.
+    },
+    devServer: {
+        overlay: true,
+        stats: "errors-only",
     },
     module: {
         rules: [
